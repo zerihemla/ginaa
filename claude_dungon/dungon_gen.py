032271f183d9859df2cd393c759fcf42133bca00
Ben Van Hoose <benvanhoose1@gmail.com>
2024-07-26 13:48:22 -0600

    fixed corridor bug

--- claude_dungon/dungon_gen.py
+++ claude_dungon/dungon_gen.py
@@ -15,11 +15,26 @@ BROWN = (165, 42, 42)
 TILE_SIZE = 20
 
 
+def create_corridor(dungeon, x1, y1, x2, y2):
+    if x1 < x2:
+        for x in range(x1, x2 + 1):
+            dungeon[y1][x] = '.'
+    elif x1 > x2:
+        for x in range(x2, x1 + 1):
+            dungeon[y1][x] = '.'
+    if y1 < y2:
+        for y in range(y1, y2 + 1):
+            dungeon[y][x2] = '.'
+    elif y1 > y2:
+        for y in range(y2, y1 + 1):
+            dungeon[y][x2] = '.'
+
+
 def create_dungeon(width, height):
     # Initialize dungeon with walls
     dungeon = [['#' for _ in range(width)] for _ in range(height)]
 
-    room_pts = {}
+    room_centers = []
     # Create rooms
     num_rooms = random.randint(5, 10)
     for _ in range(num_rooms):
@@ -27,34 +42,23 @@ def create_dungeon(width, height):
         room_height = random.randint(3, 8)
         x = random.randint(1, width - room_width - 1)
         y = random.randint(1, height - room_height - 1)
-        room_pts[x] = y
 
         for i in range(y, y + room_height):
             for j in range(x, x + room_width):
                 dungeon[i][j] = '.'
 
-    # Create corridors
-    x_vals = list(room_pts.keys())
-    y_vals = list(room_pts.values())
+        room_centers.append((x + room_width // 2, y + room_height // 2))
 
-    for i in range(len(x_vals)):
-        if i != len(x_vals) - 1:
-            x1 = x_vals[i]
-            x2 = x_vals[i + 1]
-            y1 = y_vals[i]
-            y2 = y_vals[i + 1]
-            if x1 < x2:
-                for j in range(x1, x2):
-                    dungeon[y1][j] = '.'
-            else:
-                for j in range(x2, x1):
-                    dungeon[y1][j] = '.'
-            if y1 < y2:
-                for j in range(y1, y2):
-                    dungeon[j][x2] = '.'
-            else:
-                for j in range(y2, y1):
-                    dungeon[j][x2] = '.'
+    # Create corridors
+    for i in range(len(room_centers) - 1):
+        x1, y1 = room_centers[i]
+        x2, y2 = room_centers[i + 1]
+        if random.choice([True, False]):
+            create_corridor(dungeon, x1, y1, x2, y1)
+            create_corridor(dungeon, x2, y1, x2, y2)
+        else:
+            create_corridor(dungeon, x1, y1, x1, y2)
+            create_corridor(dungeon, x1, y2, x2, y2)
 
     # Add entrance
     entrance_x = random.randint(1, width - 2)
@@ -79,32 +83,34 @@ def draw_dungeon(screen, dungeon):
                 pygame.draw.rect(screen, BROWN, rect)
             pygame.draw.rect(screen, BLACK, rect, 1)  # Draw cell borders
 
+
 def main():
     print("Welcome to the D&D Dungeon Generator!")
     width = int(input("Enter the width of the dungeon: "))
     height = int(input("Enter the height of the dungeon: "))
-    
+
     dungeon = create_dungeon(width, height)
-    
+
     # Set up the display
     screen_width = width * TILE_SIZE
     screen_height = height * TILE_SIZE
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("D&D Dungeon")
-    
+
     # Main game loop
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+
         screen.fill(BLACK)
         draw_dungeon(screen, dungeon)
         pygame.display.flip()
-    
+
     pygame.quit()
     sys.exit()
+
 
 if __name__ == "__main__":
     main()
